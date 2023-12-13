5a6dcd36add6bec93a262aa983c2bbd75d0e3dcd
rickdohmen <rickdohmen2003@gmail.com>
2023-12-13 14:22:25 +0100

    solution made while masking a new scene for a bloom effect

---
+++ HLSL-Shaders/Assets/Scripts/Bloom/BloomEffect.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class BloomEffect : MonoBehaviour
+{
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination);
+    }
+}
